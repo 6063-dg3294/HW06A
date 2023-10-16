dd081e71786ae47740c8e3bdc781856f1fac9259
Difei (Amber) Gao <dg3294@nyu.edu>
2023-10-16 01:21:24 -0400

    Initial Basics

--- sketch.js
+++ sketch.js
@@ -1,6 +1,38 @@
+let leaves = [];
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  for (let i = 0; i < 20; i++){
+    leaves.push(new Leaf(random(width), random(height)));
+  }
+  frameRate(10)
+
+
 }
 
 function draw() {
+  background(255);
+  for (let leaf of leaves){
+    leaf.display();
+    leaf.shake()
+
+  }
 }
+
+class Leaf {
+  constructor(x, y){
+    this.x = x; 
+    this.y = y;
+    this.initialY = y;
+  }
+  display() {
+    fill(50, 150, 50);
+    text("leaf", this.x, this.y);
+  }
+  
+  shake() {
+    this.y = this.initialY + 3 * sin(frameCount * 0.1 + this.x * 0.1);
+    }
+}
+
+
